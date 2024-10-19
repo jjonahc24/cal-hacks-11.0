6b84a78f809241d3ef1e30f3ae96db7b1cb3be36
David Vu <david.vu@berkeley.edu>
2024-10-19 00:49:42 -0700

    more boilerplate

--- server/app.py
+++ server/app.py
@@ -1,9 +1,21 @@
 from flask import Flask
 from flask_cors import CORS  
+from flask_pymongo import PyMongo
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+mongo = PyMongo()
 
 def create_app():
     app = Flask(__name__) 
     # when running website in localhost, add http://localhost:3000 to origins
     CORS(app)
 
+    app.config['MONGO_URI'] = os.environ.get("MONGO_URI")  # Replace with your MongoDB URI
+    print(os.environ.get("MONGO_URI"))
+
+    mongo.init_app(app)
     return app
+
+create_app()
