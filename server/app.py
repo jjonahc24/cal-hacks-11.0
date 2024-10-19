264bb381dc80b80653e7018871e8d25d971edc84
wtbui <wtbui1423@berkeley.edu>
2024-10-19 01:13:28 -0700

    setup project structure

--- server/app.py
+++ server/app.py
@@ -1,9 +1,17 @@
 from flask import Flask
-from flask_cors import CORS  
+from flask_cors import CORS
+
+import controllers
+import routes
+import models
 
 def create_app():
     app = Flask(__name__) 
+    
     # when running website in localhost, add http://localhost:3000 to origins
     CORS(app)
 
     return app
+
+create_app()
+
---
+++ server/controllers/test.py
@@ -0,0 +1,2 @@
+def test_control():
+    print("David is poop")
