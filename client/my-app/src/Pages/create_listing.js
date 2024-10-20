6cf44bd6043e3a8e1a292f68273680e514a6ecbb
Vincent <vincent.amm2@gmail.com>
2024-10-19 22:31:53 -0700

    merged david and vincent

--- client/my-app/src/Pages/create_listing.js
+++ client/my-app/src/Pages/create_listing.js
@@ -114,3 +114,4 @@ const ListingPage = () => {
 };
 
 export default ListingPage;
+
--- client/my-app/src/components/navbar/navbar.js
+++ client/my-app/src/components/navbar/navbar.js
@@ -1,17 +1,28 @@
 import React, { useState } from 'react';
 import {useNavigate} from "react-router-dom";
+import { useAuth0 } from "@auth0/auth0-react";
 
 import Profile from '../landing_page/profile';
 import ProfileDropDown from '../landing_page/profile_dropdown';
 import ProfileDropDown_SignedIn from '../landing_page/profile_dropdown_signedin';
 
-const Navbar = (props) => {
+const Navbar = () => {
+    // const [isUserAuthenthicated, setUserAuthenthicated] = useState(true);
+    // const [isSellerAuthenthicated, setSellerAuthenthicated] = useState(false);
+    const [profileToggled, setProfileToggled] = useState(false);
+    // Auth0 
+    const { user, isAuthenticated, isLoading } = useAuth0();
+
     const navigate = useNavigate();
 
     return (
         <div className="flex justify-between items-center w-full px-10 relative"> {/* Added relative to the main container */}
             <div className="flex items-center cursor-pointer" onClick={() => navigate("/")}>
                 <h2 className="font-roboto text-xl">Find⋅My⋅Spot</h2>
+                {/* {isAuthenticated ? (
+                    <h2>{user.name}</h2>
+                ) : null} */}
+                
             </div>
             
             <div className="flex items-center relative"> {/* Added relative to this div */}
@@ -20,7 +31,7 @@ const Navbar = (props) => {
                 {/* Dropdown should be absolutely positioned relative to the parent */}
                 {props.profileToggled ? (
                     <div className="absolute top-full right-0 mt-2 w-48 bg-white shadow-lg z-50"> {/* Positioned below the profile */}
-                        {props.isUserAuthenthicated ? <ProfileDropDown_SignedIn /> : <ProfileDropDown /> }
+                        {isLoading ? null : isAuthenticated ? <ProfileDropDown_SignedIn /> : <ProfileDropDown />}
                     </div>
                 ) : null}
             </div>
