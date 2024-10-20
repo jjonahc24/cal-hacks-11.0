6cae5b880b5bceb2b4140d8ebeb1d0b3bce0f902
David Vu <david.vu@berkeley.edu>
2024-10-19 17:45:29 -0700

    added input form functionality

--- client/my-app/src/components/landing_page/profile_dropdown_signedin.js
+++ client/my-app/src/components/landing_page/profile_dropdown_signedin.js
@@ -6,9 +6,8 @@ function ProfileDropDown_SignedIn() {
     <div className="flex flex-col items-center mt-2">
         <div className="bg-white w-40 border border-gray-300 rounded shadow-md pl-2">
             <button className="text-left w-full py-1 hover:bg-gray-100 text-sm mb-3 mt-3">Profile</button>
-            <button className="text-left w-full py-1 hover:bg-gray-100 text-sm mb-3">Listings</button>
-            <button className="text-left w-full py-1 hover:bg-gray-100 text-sm mb-3">Saved</button>
-            <button className="text-left w-full py-1 hover:bg-gray-100 text-sm mb-3">Help</button>
+            <button className="text-left w-full py-1 hover:bg-gray-100 text-sm mb-3">Register Listing</button>
+            
         </div>
     </div>
   );
