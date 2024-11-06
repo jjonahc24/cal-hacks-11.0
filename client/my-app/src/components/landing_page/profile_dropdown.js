a1ee20cfaa834b3b13ff53de28c717e16e3d0bd2
earnestlin123 <120564434+EarnestL@users.noreply.github.com>
2024-11-05 16:54:27 -0800

    sign in disabled

--- client/my-app/src/components/landing_page/profile_dropdown.js
+++ client/my-app/src/components/landing_page/profile_dropdown.js
@@ -9,8 +9,10 @@ function ProfileDropDown() {
     return (
       <div className="flex flex-col items-center mt-2">
         <div className="bg-white w-full border border-gray-300 rounded shadow-md">
-          <button className="text-left w-full py-1 px-2 hover:bg-gray-100 text-sm mt-3 mb-3" onClick={() => loginWithRedirect()}>Sign up</button>
-          <button className="text-left w-full py-1 px-2 hover:bg-gray-100 text-sm mb-3" onClick={() => loginWithRedirect()}>Log in</button>
+          {/*<button className="text-left w-full py-1 px-2 hover:bg-gray-100 text-sm mt-3 mb-3" onClick={() => loginWithRedirect()}>Sign up</button>*/}
+          <button className="text-left w-full py-1 px-2 text-sm mt-3 mb-3 text-gray-500 opacity-50 cursor-not-allowed" disabled>Sign up</button>
+          <button className="text-left w-full py-1 px-2 text-sm mt-3 mb-3 text-gray-500 opacity-50 cursor-not-allowed" disabled>Log in</button>
+          {/*<button className="text-left w-full py-1 px-2 hover:bg-gray-100 text-sm mb-3" onClick={() => loginWithRedirect()}>Log in</button>*/}
         </div>
       </div>
     );
